643c8acc5f624ef254320a5bc5c25b3b0f21c019
Mike Hiersche <hierschemichael@gmail.com>
2023-07-18 12:36:09 -0400

    modified:   Office 365/1 Global/Azure Graph.ps1 	new file:   Office 365/1 Global/SharePoint Online.ps1 	modified:   README.md

--- Office 365/1 Global/Azure Graph.ps1
+++ Office 365/1 Global/Azure Graph.ps1
@@ -1,3 +1,4 @@
+# Azure Graph is using to connect to Microsoft Azure AD, or Entra ID, then run commands against objects in Azure AD (Users, Groups, etc)
 # Install the module
 Install-Module -Name Microsoft.Graph  
 
@@ -6,3 +7,6 @@ Update-Module -Name Microsoft.Graph
 
 # Import the module (should not usually be needed)
 Import-Module -Name Microsoft.Graph
+
+# Here is a link to a graph with a list of Azure AD commandlets and the Graph counterparts.
+## https://learn.microsoft.com/en-us/powershell/microsoftgraph/azuread-msoline-cmdlet-map
