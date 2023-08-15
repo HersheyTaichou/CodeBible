3b5fe2b6c4833f2d82b6e634d827b8884841a0f5
Mike Hiersche <hierschemichael@gmail.com>
2023-08-15 16:13:36 -0400

    Adds .gitignore and includes work on 365Connect

--- Powershell/Modules/365Modules/365Connect.psm1
+++ Powershell/Modules/365Modules/365Connect.psm1
@@ -22,29 +22,42 @@
    Update-365Modules -AddModules "Az","AzureAD"
 #>
 $Script:Customers = @()
-class Planet : System.Management.Automation.IValidateSetValuesGenerator {
+class Customers : System.Management.Automation.IValidateSetValuesGenerator {
     [String[]] GetValidValues() {
-        Write-Host "Please log into the Partner Center"
-        Connect-PartnerCenter
-        $Script:Customers = Get-PartnerCustomer
+        If (Test-Path "customers.json") {
+            $Script:Customers = Get-Content -Raw customers.json | Convertfrom-Json
+        } else {
+
+        }
         Return ($Script:Customers).Name
     }
 }
 
-function Connect-Service {
-    param (
-        [Parameter(Mandatory=$true)] [String]$Module,
-        [Parameter(Mandatory=$true)] [ValidateSet([Customers])] [array]$Client
-    )
-    <#
-    if ($Module -eq "ExchangeOnline") {
-        Write-Host "Please log in to Exchange Online"
-        Connect-ExchangeOnline -DelegatedOrganization $Client
-    }elseif ($Module -eq "MgGraph") {
-        Connect-MgGraph -TenantId $Client
-    }
-    #>
-    write-host $client
+function Get-AvailableCustomers {
+    Write-Host "Please log into the Partner Center"
+    Connect-PartnerCenter
+    $Script:Customers = Get-PartnerCustomer
+    $Script:Customers | ConvertTo-Json | Out-File customers.json
+        
 }
 
-Export-ModuleMember -Function Connect-Service
+function Connect-Service {
+    param (
+        [Parameter(Mandatory=$true)] 
+        [ValidateSet("ExchangeOnline","MgGraph")]
+            [String]$Module,
+        [Parameter(Mandatory=$true)] [ValidateSet([Customers])] [array]$Client
+    )
+        if ($Module -eq "ExchangeOnline") {
+        Write-Host "Please log in to Exchange Online"
+        $Domain = ($Script:Customers | Where-Object {$_.Name -eq "$Client"}).Domain
+        Write-Host $CustomerId
+        Connect-ExchangeOnline -DelegatedOrganization $Domain
+    }elseif ($Module -eq "MgGraph") {
+        #Connect-MgGraph -TenantId $CustomerId
+        $CustomerId = ($Script:Customers | Where-Object {$_.Name -eq "$Client"}).CustomerId
+        Write-Host $CustomerId
+    }
+}
+
+Export-ModuleMember -Function Connect-Service, Get-AvailableCustomers
