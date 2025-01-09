591a21b046b1d0750aa27e24264a434b6ba34a56
Mike Hiersche <hierschemichael@gmail.com>
2025-01-09 16:09:37 -0500

    Remove custom domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "docs.oriontech.info",
+    baseUrl: "hersheytaichou.github.io/CodeBible",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
